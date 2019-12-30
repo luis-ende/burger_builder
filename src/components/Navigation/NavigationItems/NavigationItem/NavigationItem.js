2c426938e0c55d210d0b50190414c09729ef21aa
Luis Salazar <lsalazar@gmx.de>
2019-12-30 12:10:51 -0600

    first complete prototype

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import classes from './NavigationItem.css';
 
-const navigationItem = (props) => {
-    return (
-        <li className={classes.NavigationItem}>
-            <a href={props.link}
+const navigationItem = (props) => (
+    <li className={classes.NavigationItem}>
+        <a href={props.link}
             className={props.active ? classes.active : null}>{props.children}</a>
-        </li>
-    )
-}
+    </li>
+);
 
 export default navigationItem;
